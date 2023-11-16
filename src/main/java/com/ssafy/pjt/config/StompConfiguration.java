0108b798fd5ea1b88e8b029bb5fbfce8cbb1f938
hyundol2 <nhs503601@gmail.com>
2023-11-17 01:56:48 +0900

    aaa

--- src/main/java/com/ssafy/pjt/config/StompConfiguration.java
+++ src/main/java/com/ssafy/pjt/config/StompConfiguration.java
@@ -5,35 +5,35 @@ import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import com.ssafy.pjt.chat.StompChatHandler;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
-@EnableWebSocket
-@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
 public class StompConfiguration implements WebSocketMessageBrokerConfigurer  {
 
-    private final StompChatHandler stompHandler;
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*");
-        // .withSockJS();
-    }
-
-    @Override
+    // spring 내장 broker 설정
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/queue", "/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+        // 메시지를 구독하는 요청 url => 즉 메시지 받을 때
+        registry.enableSimpleBroker("/sub");
+
+        // 메시지를 발행하는 요청 url => 즉 메시지 보낼 때
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-//        registration.interceptors(stompHandler);
+    // 웹소켓 연결 주소 설정
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // stomp 접속 주소 url => /ws-stomp
+        registry.addEndpoint("/ws") // 연결될 엔드포인트
+		        .setAllowedOriginPatterns("*")        
+		        .withSockJS(); // SocketJS 를 연결한다는 설정
     }
+    
 }
