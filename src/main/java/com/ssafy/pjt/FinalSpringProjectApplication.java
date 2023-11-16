0abbc7d8abcaa6984f0371354af7eb5187a1af1d
이태경 <korea9300@naver.com>
2023-11-16 11:16:26 +0900

    first commit

---
+++ src/main/java/com/ssafy/pjt/FinalSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSpringProjectApplication.class, args);
+	}
+
+}
