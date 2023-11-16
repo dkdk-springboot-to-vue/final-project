feedd2908312ff64878fd6dac4ec5ccdc4a5a639
junyoung-choe <wjdghkswnsdu@naver.com>
2023-11-16 17:55:26 +0900

    hj1116

---
+++ frontend/src/components/Member/MemberLogin.vue
@@ -0,0 +1,220 @@
+<template>
+    <div>
+        <h2>Modal Login Form</h2>
+        <button @click="toggleLoginForm" style="width:auto;">Login</button>
+
+        <div v-if="isLoginFormVisible" class="modal" @click="closeLoginForm">
+            <form class="modal-content animate" @click.stop>
+                <div class="imgcontainer">
+                    <span @click="closeLoginForm" class="close" title="Close Modal">&times;</span>
+                    <img src="@/assets/img/velkoz.jpg" alt="Avatar" class="avatar">
+                </div>
+
+                <div class="container">
+                    <label for="uname"><b>Username</b></label>
+                    <input v-model="formData.username" type="text" placeholder="Enter Username" name="uname" required>
+
+                    <label for="psw"><b>Password</b></label>
+                    <input v-model="formData.password" type="password" placeholder="Enter Password" name="psw" required>
+
+                    <button type="button" @click="login">Login</button>
+                    <label>
+                        <input type="checkbox" v-model="formData.remember"> Remember me
+                    </label>
+                </div>
+
+                <div class="container" style="background-color:#f1f1f1">
+                    <button type="button" @click="closeLoginForm" class="cancelbtn">Cancel</button>
+                    <span class="psw">Forgot <a href="#">password?</a></span>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const isLoginFormVisible = ref(false);
+        const formData = ref({
+            username: '',
+            password: '',
+            remember: false,
+        });
+
+        const toggleLoginForm = () => {
+            isLoginFormVisible.value = !isLoginFormVisible.value;
+        };
+
+        const closeLoginForm = () => {
+            isLoginFormVisible.value = false;
+        };
+
+        const login = () => {
+            // Add your login logic here
+            console.log('Login logic goes here', formData.value);
+            closeLoginForm();
+        };
+
+        return {
+            isLoginFormVisible,
+            formData,
+            toggleLoginForm,
+            closeLoginForm,
+            login,
+        };
+    },
+};
+</script>
+<style scoped>
+/* Container for the component */
+.container {
+    text-align: center;
+}
+
+/* Login button styling */
+button {
+    background-color: #04AA6D;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: auto;
+    font-size: 16px;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+/* Modal styles */
+.modal {
+    display: flex;
+    /* Use flex to center the modal content */
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+/* Modal content box */
+.modal-content {
+    background-color: #fefefe;
+    padding: 16px;
+    /* Add padding to modal content */
+    border: 1px solid #888;
+    width: 60%;
+    max-width: 400px;
+    border-radius: 10px;
+    text-align: left;
+    /* Align text to the left */
+}
+
+/* Close button styling */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+}
+
+/* Image container styling */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+
+/* Avatar image styling */
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+/* Form container styling */
+.form-container {
+    padding: 16px;
+}
+
+/* Form label styling */
+label {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 8px;
+    color: #333;
+}
+
+/* Form input styling */
+input {
+    width: 100%;
+    padding: 12px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    border-radius: 5px;
+}
+
+/* Submit button styling */
+button[type="submit"] {
+    background-color: #04AA6D;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 5px;
+    font-size: 16px;
+}
+
+button[type="submit"]:hover {
+    opacity: 0.8;
+}
+
+/* Remember me checkbox styling */
+label input[type="checkbox"] {
+    margin-top: 8px;
+}
+
+/* Cancel button styling */
+.cancelbtn {
+    width: 100%;
+    padding: 10px 18px;
+    background-color: #f44336;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    cursor: pointer;
+}
+
+.cancelbtn:hover {
+    opacity: 0.8;
+}
+
+/* Password link styling */
+.psw a {
+    color: #04AA6D;
+}
+
+.psw a:hover {
+    text-decoration: underline;
+}
+</style>
---
+++ frontend/src/views/FormRow.vue
@@ -0,0 +1,63 @@
+<!-- FormRow.vue -->
+<template>
+    <div class="form-row">
+        <label class="label">{{ label }}</label>
+        <div class="input-container">
+            <input :type="type" :value="value" @input="$emit('update:modelValue', $event)" />
+        </div>
+        <p class="error-text">{{ error }}</p>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        label: String,
+        type: {
+            type: String,
+            default: 'text',
+        },
+        value: String,
+        error: String,
+    },
+};
+</script>
+  
+<style scoped>
+.form-row {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+
+.label {
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 8px;
+    color: #333;
+}
+
+.input-container {
+    width: 100%;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    box-sizing: border-box;
+    transition: border-color 0.3s;
+}
+
+input:focus {
+    outline: none;
+    border-color: #4a90e2;
+}
+
+.error-text {
+    margin-top: 8px;
+    font-size: 14px;
+    color: red;
+}
+</style>
